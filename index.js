ec64d561068597cf63e65c20983f074235847ff8
Tim Ruser <tim@ruser.dev>
2022-08-21 21:40:36 +0200

    Worked on installation-popup

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
-// ----- Install ----- 
-// Creating Install Component
-// Rendering Component
-const domContainer = document.querySelector('#like_button_container');
-const root = ReactDOM.createRoot(domContainer);
-root.render(Install);
+const root = ReactDOM.createRoot(document.getElementById('react-install-root'));
+
+function Install(props) {
+  return /*#__PURE__*/React.createElement("div", {
+    className: "installation-popup-root"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "installation-popup"
+  }, /*#__PURE__*/React.createElement("i", {
+    className: "fa-solid fa-xmark fa-2x installation-popup-x",
+    onClick: closeInstall
+  }), /*#__PURE__*/React.createElement("div", {
+    className: "installation-popup-content"
+  }, /*#__PURE__*/React.createElement("h1", null, props.os), /*#__PURE__*/React.createElement("p", null, "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis fuga, necessitatibus ", /*#__PURE__*/React.createElement("br", null), " mollitia pariatur ipsam iusto ad voluptatum libero? Pariatur nobis, iure perspiciatis vero ullam reiciendis fugit? Maxime ", /*#__PURE__*/React.createElement("br", null), " aspernatur laudantium sapiente?"))), /*#__PURE__*/React.createElement("div", {
+    className: "installation-popup-background",
+    onClick: closeInstall
+  }));
+}
+
+function openInstall(os) {
+  // Creating Install Component
+  let element = /*#__PURE__*/React.createElement(Install, {
+    os: os
+  });
+  console.log("It worked!"); // Rendering Component
+
+  const rootElement = document.getElementById('react-install-root');
+  rootElement.style.visibility = 'visible';
+  root.render(element);
+}
+
+function closeInstall() {
+  let element = /*#__PURE__*/React.createElement("div", null);
+  const rootElement = document.getElementById('react-install-root');
+  rootElement.style.visibility = 'hidden';
+  root.render(element);
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,38 @@
+const root = ReactDOM.createRoot(document.getElementById('react-install-root'))
 
+function Install(props) {
+    return <div className="installation-popup-root">
+        <div className="installation-popup" >
+            <i className="fa-solid fa-xmark fa-2x installation-popup-x" onClick={closeInstall}></i>
+            <div className="installation-popup-content">
+                <h1>{props.os}</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis fuga, necessitatibus <br /> mollitia pariatur ipsam iusto ad voluptatum libero? Pariatur nobis, iure perspiciatis vero ullam reiciendis fugit? Maxime <br /> aspernatur laudantium sapiente?</p>
+                
+            </div>
+        </div>
+        <div className="installation-popup-background" onClick={closeInstall}></div>
+    </div>
+}
 
+function openInstall(os) {
+    // Creating Install Component
+    let element = (
+        <Install os={os} />
+    )
+    console.log("It worked!")
 
+    // Rendering Component
+    const rootElement = document.getElementById('react-install-root')
+    rootElement.style.visibility = 'visible';
+        
+    root.render(element)
+}
 
+function closeInstall() {
+    let element = <div></div>;
+    
+    const rootElement = document.getElementById('react-install-root')
+    rootElement.style.visibility = 'hidden';
 
-// ----- Install ----- 
-
-// Creating Install Component
-
-// Rendering Component
-
-const domContainer = document.querySelector('#like_button_container');
-const root = ReactDOM.createRoot(domContainer);
-root.render(Install);
+    root.render(element);
+}
