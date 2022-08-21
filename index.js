e773d059b1fbc1a133f87dee1daabdaee6963f0f
Tim Ruser <tim@ruser.dev>
2022-08-21 19:13:52 +0200

    Added babel and changed installation and added react

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+// ----- Install ----- 
+
+// Creating Install Component
+
+// Rendering Component
+
+const domContainer = document.querySelector('#like_button_container');
+const root = ReactDOM.createRoot(domContainer);
+root.render(Install);
