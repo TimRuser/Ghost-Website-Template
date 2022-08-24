2d96d9f695304b2c9668fbc141621040d0d330ea
Tim Ruser <tim@ruser.dev>
2022-08-24 08:21:04 +0200

    Changed react tag to production.min.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 const root = ReactDOM.createRoot(document.getElementById('react-install-root'))
 
+let windows = <p>This is the windows text.</p>;
+let mac = <p>This is the mac text</p>;
+let linux = <p>This is the linux text</p>;
+let text;
+
+switch (props.os) {
+    case 'Windows':
+        text = windows;
+        break;
+    case 'Linux':
+        text = linux;
+        break;
+    case 'MacOS':
+        text = mac;
+    default:
+        break;
+}
+
 function Install(props) {
     return <div className="installation-popup-root">
         <div className="installation-popup" >
             <i className="fa-solid fa-xmark fa-2x installation-popup-x" onClick={closeInstall}></i>
             <div className="installation-popup-content">
-                <h1>{props.os}</h1>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis fuga, necessitatibus <br /> mollitia pariatur ipsam iusto ad voluptatum libero? Pariatur nobis, iure perspiciatis vero ullam reiciendis fugit? Maxime <br /> aspernatur laudantium sapiente?</p>
-                
+                <h1>Install software on {props.os}</h1>
+                {text}
             </div>
         </div>
         <div className="installation-popup-background" onClick={closeInstall}></div>
